4a55b3eaae8b1e53332373fb99c9697d8a07b3d2
rknabe <rusty.knabe@gmail.com>
2024-01-27 22:59:01 -0600

    initial work generic spring effect

--- AFFBWheel/FfbEngine.cpp
+++ AFFBWheel/FfbEngine.cpp
@@ -26,7 +26,6 @@
 #include "HIDReportType.h"
 
 FfbEngine::FfbEngine() {
-
 }
 
 void FfbEngine::SetFfb(FfbReportHandler* reporthandler) {
@@ -35,37 +34,34 @@ void FfbEngine::SetFfb(FfbReportHandler* reporthandler) {
 
 
 //returns 15-bit force value in range [-16384..16384]
-int16_t FfbEngine::calculateForce(AxisWheel* axis)
-{
+int16_t FfbEngine::calculateForce(AxisWheel* axis) {
   int32_t totalForce = 0;
   int16_t tmpForce;
 
   volatile TEffectState* effect;
 
-  int16_t _millis=millis();
-  int16_t timeDiff=_millis-prevTime;
-  prevTime=_millis;
+  int16_t _millis = millis();
+  int16_t timeDiff = _millis - prevTime;
+  prevTime = _millis;
 
-  for (uint8_t id = 0; id <= MAX_EFFECTS; id++)
-  {
+  for (uint8_t id = 0; id <= MAX_EFFECTS; id++) {
     effect = &ffbReportHandler->gEffectStates[id];
-    tmpForce=0;
+    tmpForce = 0;
+
+    if (effect->effectType == USB_EFFECT_SPRING /**TODO** and config is on**/) {
+      effect->state = MEFFECTSTATE_PLAYING;
+    }
 
     //stop effect if it reached duration
-    if ((effect->state & MEFFECTSTATE_PLAYING) && !ffbReportHandler->devicePaused)
-    {
-      
-      if ((effect->duration != USB_DURATION_INFINITE) && (effect->elapsedTime+timeDiff > effect->duration))
-      {
+    if ((effect->state & MEFFECTSTATE_PLAYING) && !ffbReportHandler->devicePaused) {
+
+      if ((effect->duration != USB_DURATION_INFINITE) && (effect->elapsedTime + timeDiff > effect->duration)) {
         ffbReportHandler->StopEffect(id);
-      }
-      else
-      {
+      } else {
         if (effect->duration != USB_DURATION_INFINITE)
           effect->elapsedTime += timeDiff;
-          
-        switch (effect->effectType)
-        {
+
+        switch (effect->effectType) {
           case USB_EFFECT_CONSTANT:
             tmpForce = constantForce(effect);
             break;
@@ -94,75 +90,65 @@ int16_t FfbEngine::calculateForce(AxisWheel* axis)
             tmpForce = frictionForce(effect, axis->velocity);
             break;
         }
-        
-        //applying effect gains
-        if ((effect->gain==0)||(settings.gain[effect->effectType]==0))
-          tmpForce = 0;
-        else
-        {
-          
-          if (effect->gain!=255)
-            tmpForce = ((int32_t)tmpForce * (effect->gain+1)) >> 8;
 
-          if (settings.gain[effect->effectType]!=1024)
+        //applying effect gains
+        if ((effect->gain == 0) || (settings.gain[effect->effectType] == 0))
+          tmpForce = 0;
+        else {
+
+          if (effect->gain != 255)
+            tmpForce = ((int32_t)tmpForce * (effect->gain + 1)) >> 8;
+
+          if (settings.gain[effect->effectType] != 1024)
             tmpForce = applyGain(tmpForce, settings.gain[effect->effectType]);
 
           tmpForce = constrain(tmpForce, -16383, 16383);
         }
 
-        totalForce+=tmpForce;
-        
-      }  
+        totalForce += tmpForce;
+      }
     }
   }
 
   //actuators disabled
-  if (!(ffbReportHandler->pidState.status & 2 ) || (!totalForce))
+  if (!(ffbReportHandler->pidState.status & 2) || (!totalForce))
     return 0;
 
   //applying global gains
-  if ((ffbReportHandler->deviceGain==0) || (settings.gain[GAIN_TOTAL]==0))
+  if ((ffbReportHandler->deviceGain == 0) || (settings.gain[GAIN_TOTAL] == 0))
     return 0;
-  else
-  {
-    if (ffbReportHandler->deviceGain!=255)
-      totalForce = (totalForce * (ffbReportHandler->deviceGain +1)) >> 8;
+  else {
+    if (ffbReportHandler->deviceGain != 255)
+      totalForce = (totalForce * (ffbReportHandler->deviceGain + 1)) >> 8;
 
-    if (settings.gain[GAIN_TOTAL]!=1024)
+    if (settings.gain[GAIN_TOTAL] != 1024)
       totalForce = applyGain(totalForce, settings.gain[GAIN_TOTAL]);
   }
 
   return constrain(totalForce, -16383, 16383);
 }
 
-int16_t FfbEngine::constantForce(volatile TEffectState*  effect)
-{
-    return  envelope(effect);
+int16_t FfbEngine::constantForce(volatile TEffectState* effect) {
+  return envelope(effect);
 }
 
-int16_t FfbEngine::rampForce(volatile TEffectState*  effect)
-{
-  int16_t magnitude=envelope(effect);
-  
-  if (effect->duration==USB_DURATION_INFINITE)
-  {
+int16_t FfbEngine::rampForce(volatile TEffectState* effect) {
+  int16_t magnitude = envelope(effect);
+
+  if (effect->duration == USB_DURATION_INFINITE) {
     return effect->offset - magnitude;
-  }
-  else
-  {
-    return effect->offset + (int32_t)((int32_t)magnitude * effect->elapsedTime * effect->periodC ) - magnitude;
+  } else {
+    return effect->offset + (int32_t)((int32_t)magnitude * effect->elapsedTime * effect->periodC) - magnitude;
   }
 }
 
-int32_t FfbEngine::periodicForce(volatile TEffectState* effect)
-{
-  while (effect->periodTime>effect->period)
-    effect->periodTime-=effect->period;
-    
-  int16_t magnitude=envelope(effect);
-  
-  switch (effect->effectType)
-  {
+int32_t FfbEngine::periodicForce(volatile TEffectState* effect) {
+  while (effect->periodTime > effect->period)
+    effect->periodTime -= effect->period;
+
+  int16_t magnitude = envelope(effect);
+
+  switch (effect->effectType) {
     case USB_EFFECT_SQUARE:
       return effect->offset + square(effect, magnitude);
     case USB_EFFECT_SINE:
@@ -178,70 +164,57 @@ int32_t FfbEngine::periodicForce(volatile TEffectState* effect)
   return 0;
 }
 
-int16_t FfbEngine::envelope(volatile TEffectState* effect)
-{
-    if (effect->attackTime)
-    if (effect->elapsedTime < effect->attackTime)
-    {
-       return sign(effect->magnitude) *(effect->attackLevel + (int32_t)(abs(effect->magnitude) - (int16_t)effect->attackLevel) * (int16_t)effect->elapsedTime * effect->attackTimeC);
+int16_t FfbEngine::envelope(volatile TEffectState* effect) {
+  if (effect->attackTime)
+    if (effect->elapsedTime < effect->attackTime) {
+      return sign(effect->magnitude) * (effect->attackLevel + (int32_t)(abs(effect->magnitude) - (int16_t)effect->attackLevel) * (int16_t)effect->elapsedTime * effect->attackTimeC);
     }
 
-    if (effect->duration!=USB_DURATION_INFINITE)
+  if (effect->duration != USB_DURATION_INFINITE)
     if (effect->fadeTime)
-    if (effect->elapsedTime > effect->duration - effect->fadeTime)
-    {
-       return sign(effect->magnitude) * (effect->fadeLevel + (int32_t)(abs(effect->magnitude) - (int16_t)effect->fadeLevel) * (int16_t)(effect->duration-effect->elapsedTime) * effect->fadeTimeC);
-    }
+      if (effect->elapsedTime > effect->duration - effect->fadeTime) {
+        return sign(effect->magnitude) * (effect->fadeLevel + (int32_t)(abs(effect->magnitude) - (int16_t)effect->fadeLevel) * (int16_t)(effect->duration - effect->elapsedTime) * effect->fadeTimeC);
+      }
 
-    return effect->magnitude;
-}
-  
-int16_t FfbEngine::square(volatile TEffectState*  effect, int16_t magnitude)
-{
-    if (effect->periodTime < effect->halfPeriod)
-      return magnitude;
-    else
-      return -magnitude;
+  return effect->magnitude;
 }
 
-int16_t FfbEngine::sinefix(volatile TEffectState*  effect, int16_t magnitude)
-{
-    return (int32_t)magnitude * sin_fix(effect->periodTime * effect->periodC) >>14;
+int16_t FfbEngine::square(volatile TEffectState* effect, int16_t magnitude) {
+  if (effect->periodTime < effect->halfPeriod)
+    return magnitude;
+  else
+    return -magnitude;
 }
 
-int16_t FfbEngine::triangle(volatile TEffectState*  effect, int32_t magnitude)
-{
-    if (effect->periodTime < effect->halfPeriod)
-      return -magnitude + (int32_t)((int32_t)magnitude * effect->periodTime * effect->periodC);
-    else
-      return 3 * magnitude - (int32_t)((int32_t)magnitude * effect->periodTime * effect->periodC);
+int16_t FfbEngine::sinefix(volatile TEffectState* effect, int16_t magnitude) {
+  return (int32_t)magnitude * sin_fix(effect->periodTime * effect->periodC) >> 14;
 }
 
-int16_t FfbEngine::stdown(volatile TEffectState*  effect, int32_t magnitude)
-{
-    return magnitude - (int32_t)((int32_t)magnitude * effect->periodTime * effect->periodC);
-} 
+int16_t FfbEngine::triangle(volatile TEffectState* effect, int32_t magnitude) {
+  if (effect->periodTime < effect->halfPeriod)
+    return -magnitude + (int32_t)((int32_t)magnitude * effect->periodTime * effect->periodC);
+  else
+    return 3 * magnitude - (int32_t)((int32_t)magnitude * effect->periodTime * effect->periodC);
+}
 
-int16_t FfbEngine::springForce(volatile TEffectState*  effect, int16_t position) 
-{
-  int32_t  tempForce;
+int16_t FfbEngine::stdown(volatile TEffectState* effect, int32_t magnitude) {
+  return magnitude - (int32_t)((int32_t)magnitude * effect->periodTime * effect->periodC);
+}
 
-  position = position >>1;
+int16_t FfbEngine::springForce(volatile TEffectState* effect, int16_t position) {
+  int32_t tempForce;
 
-  if (position < (effect->cpOffset - (int16_t)effect->deadBand)) 
-  {
+  position = position >> 1;
+
+  if (position < (effect->cpOffset - (int16_t)effect->deadBand)) {
     tempForce = ((position - (effect->cpOffset - (int32_t)effect->deadBand)) * effect->negativeCoefficient) >> 14;
     tempForce = constrain(tempForce, -(int16_t)effect->negativeSaturation, effect->negativeSaturation);
-  }
-  else 
-  if (position > (effect->cpOffset + (int16_t)effect->deadBand)) 
-  {
+  } else if (position > (effect->cpOffset + (int16_t)effect->deadBand)) {
     tempForce = ((position - (effect->cpOffset + (int32_t)effect->deadBand)) * effect->positiveCoefficient) >> 14;
     tempForce = constrain(tempForce, -(int16_t)effect->positiveSaturation, effect->positiveSaturation);
-  }
-  else
+  } else
     return 0;
-  
+
   return tempForce;
 }
 
@@ -260,27 +233,21 @@ int16_t FfbEngine::springForce(volatile TEffectState*  effect, int16_t position)
  * damper creates force agains velocity. 
  * Maximum value for velocity is needed for scaling, so it is set with maxVelocityDamper
  */
-int16_t FfbEngine::damperForce(volatile TEffectState*  effect, int16_t velocity) 
-{
-    int32_t  tempForce;
+int16_t FfbEngine::damperForce(volatile TEffectState* effect, int16_t velocity) {
+  int32_t tempForce;
 
-    velocity= velocity * maxVelocityDamperC;
-    
-    if (velocity < (effect->cpOffset - (int16_t)effect->deadBand)) 
-    {
-        tempForce = ((velocity - (effect->cpOffset - (int32_t)effect->deadBand)) * effect->negativeCoefficient) >>14;
-        tempForce = constrain(tempForce, -(int16_t)effect->negativeSaturation, effect->negativeSaturation);
-    }
-    else 
-    if (velocity > (effect->cpOffset + (int16_t)effect->deadBand)) 
-    {
-        tempForce = ((velocity - (effect->cpOffset + (int32_t)effect->deadBand)) * effect->positiveCoefficient)>>14;
-        tempForce = constrain(tempForce, -(int16_t)effect->positiveSaturation, effect->positiveSaturation);
-    }
-    else
-      return 0;
+  velocity = velocity * maxVelocityDamperC;
 
-    return tempForce;
+  if (velocity < (effect->cpOffset - (int16_t)effect->deadBand)) {
+    tempForce = ((velocity - (effect->cpOffset - (int32_t)effect->deadBand)) * effect->negativeCoefficient) >> 14;
+    tempForce = constrain(tempForce, -(int16_t)effect->negativeSaturation, effect->negativeSaturation);
+  } else if (velocity > (effect->cpOffset + (int16_t)effect->deadBand)) {
+    tempForce = ((velocity - (effect->cpOffset + (int32_t)effect->deadBand)) * effect->positiveCoefficient) >> 14;
+    tempForce = constrain(tempForce, -(int16_t)effect->positiveSaturation, effect->positiveSaturation);
+  } else
+    return 0;
+
+  return tempForce;
 }
 
 
@@ -288,36 +255,30 @@ int16_t FfbEngine::damperForce(volatile TEffectState*  effect, int16_t velocity)
  * Inertia effect should oppose acceleration.
  * maximum value for acceleration is set with maxAccelerationInertia
 */
-int16_t FfbEngine::inertiaForce(volatile TEffectState*  effect, AxisWheel* axis) 
-{
-    int32_t  tempForce;
-    
-    //inertia effect works only for deceleration
-    if (sign(axis->acceleration)==sign(axis->velocity))
-      return 0;
-      
-    int16_t acceleration = axis->acceleration * maxAccelerationInertiaC;
-   
-    if (acceleration < (effect->cpOffset - (int16_t)effect->deadBand)) 
-    {
-        tempForce = ((acceleration - (effect->cpOffset - (int32_t)effect->deadBand)) * effect->negativeCoefficient)>>14;
+int16_t FfbEngine::inertiaForce(volatile TEffectState* effect, AxisWheel* axis) {
+  int32_t tempForce;
 
-        //Saturation ignored if is 0
-        if (effect->negativeSaturation)
-          tempForce = constrain(tempForce, -(int16_t)effect->negativeSaturation, effect->negativeSaturation);
-    }
-    else 
-    if (acceleration > (effect->cpOffset + (int16_t)effect->deadBand)) 
-    {
-        tempForce = ((acceleration - (effect->cpOffset + (int32_t)effect->deadBand)) * effect->positiveCoefficient)>>14;
-      
-        if (effect->positiveSaturation)
-          tempForce = constrain(tempForce, -(int16_t)effect->positiveSaturation, effect->positiveSaturation);
-    }
-    else
-      return 0;
+  //inertia effect works only for deceleration
+  if (sign(axis->acceleration) == sign(axis->velocity))
+    return 0;
 
-    return tempForce;
+  int16_t acceleration = axis->acceleration * maxAccelerationInertiaC;
+
+  if (acceleration < (effect->cpOffset - (int16_t)effect->deadBand)) {
+    tempForce = ((acceleration - (effect->cpOffset - (int32_t)effect->deadBand)) * effect->negativeCoefficient) >> 14;
+
+    //Saturation ignored if is 0
+    if (effect->negativeSaturation)
+      tempForce = constrain(tempForce, -(int16_t)effect->negativeSaturation, effect->negativeSaturation);
+  } else if (acceleration > (effect->cpOffset + (int16_t)effect->deadBand)) {
+    tempForce = ((acceleration - (effect->cpOffset + (int32_t)effect->deadBand)) * effect->positiveCoefficient) >> 14;
+
+    if (effect->positiveSaturation)
+      tempForce = constrain(tempForce, -(int16_t)effect->positiveSaturation, effect->positiveSaturation);
+  } else
+    return 0;
+
+  return tempForce;
 }
 
 
@@ -329,43 +290,34 @@ int16_t FfbEngine::inertiaForce(volatile TEffectState*  effect, AxisWheel* axis)
  * 
  * cpOffset and saturation ignored
  */
-int16_t FfbEngine::frictionForce(volatile TEffectState*  effect, int16_t velocity) 
-{
-       
-    if (velocity==0)
-      return 0;
-   
-    velocity = velocity * maxVelocityFrictionC;
+int16_t FfbEngine::frictionForce(volatile TEffectState* effect, int16_t velocity) {
 
-    int16_t coefficient,tVelocity;
-
-    if (velocity>0)
-    {
-      coefficient=effect->positiveCoefficient;
-      tVelocity=velocity-effect->deadBand;
-    }
-    else
-    {
-      coefficient=-effect->negativeCoefficient;
-      tVelocity=-velocity-effect->deadBand;
-    }
-
-    if (tVelocity > 256)
-    {
-      return coefficient;
-    }
-    else
-    if (tVelocity>0)
-    {
-      return ((int32_t)tVelocity * coefficient) >> 8;
-    }
-
+  if (velocity == 0)
     return 0;
+
+  velocity = velocity * maxVelocityFrictionC;
+
+  int16_t coefficient, tVelocity;
+
+  if (velocity > 0) {
+    coefficient = effect->positiveCoefficient;
+    tVelocity = velocity - effect->deadBand;
+  } else {
+    coefficient = -effect->negativeCoefficient;
+    tVelocity = -velocity - effect->deadBand;
+  }
+
+  if (tVelocity > 256) {
+    return coefficient;
+  } else if (tVelocity > 0) {
+    return ((int32_t)tVelocity * coefficient) >> 8;
+  }
+
+  return 0;
 }
 
-int16_t applyGain(int32_t force, int16_t gain)
-{
-    force=force * gain;
-    force=constrain(force, -16383L<<10, 16383L<<10);
-    return force >> 10;
+int16_t applyGain(int32_t force, int16_t gain) {
+  force = force * gain;
+  force = constrain(force, -16383L << 10, 16383L << 10);
+  return force >> 10;
 }
