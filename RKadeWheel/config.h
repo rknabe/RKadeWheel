161478cc76c07b94d345e95a27704d7e8bf16f56
rknabe <rusty.knabe@gmail.com>
2024-05-24 20:47:30 -0500

    change afc defaults

--- RKadeWheel/config.h
+++ RKadeWheel/config.h
@@ -211,7 +211,7 @@
 //Auto find center
 #define AFC_ON           //Uncomment to enable autofind center at start. Requires presence of mechanical limiters (see description)
 #define AFC_FORCE 4200   //Force [0...16383] to exert when finding center.
-#define AFC_PERIOD 50    //default 50         //Position check period in milliseconds
+#define AFC_PERIOD 80    //default 50         //Position check period in milliseconds
 #define AFC_TRESHOLD 10  //default 10         //Minimum position change to detect movement
 #define AFC_NORANGE      //Uncomment to disable range setting
 #define AFC_RANGE_FIX 5  //range will be decreased by this value (in degrees), to prevent wheel kicking on limiters.
