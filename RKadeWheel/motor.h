f83caf31c1f4e9abb6b78b0ef4f08c9f2138ab94
rknabe <rusty.knabe@gmail.com>
2025-06-24 23:34:54 -0500

    cleanup

--- RKadeWheel/motor.h
+++ RKadeWheel/motor.h
@@ -2,15 +2,13 @@
 
 #include <Arduino.h>
 #include "config.h"
-//#include <digitalWriteFast.h>
 
-class Motor
-{
-  public:
-    uint8_t bitDepth;
-    void begin();
-    void setForce(int16_t value);
-    void setBitDepth(uint8_t value);
-  private:
-    uint8_t bitShift;
+class Motor {
+public:
+  uint8_t bitDepth;
+  void begin();
+  void setForce(int16_t value);
+  void setBitDepth(uint8_t value);
+private:
+  uint8_t bitShift;
 };
